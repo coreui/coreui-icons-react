6c77a3128ad68cc66b3233baa3634276be05ea29
Łukasz Holeczek <lukasz@holeczek.pl>
2021-07-05 12:27:06 +0200

    release: v2.0.0-rc.0

---
+++ dist/CIcon.d.ts
@@ -0,0 +1,33 @@
+import React, { DOMAttributes } from 'react';
+import './CIcon.css';
+export interface CIconProps extends DOMAttributes<SVGSVGElement> {
+    /**
+     * A string of all className you want applied to the component. [docs]
+     */
+    className?: string;
+    /**
+     * Name of the icon placed in React object. [docs]
+     */
+    name?: string;
+    /**
+     * Icon SVG content. [docs]
+     */
+    content?: string | Array<string>;
+    /**
+     * Size of the icon. Available sizes: 'sm', 'lg', 'xl', '2xl'...'9xl', 'custom', 'custom-size'. [docs]
+     */
+    size?: 'custom-size' | 'sm' | 'lg' | 'xl' | '2xl' | '3xl' | '4xl' | '5xl' | '6xl' | '7xl' | '8xl' | '9xl';
+    /**
+     * Use for replacing default CIcon component classes. Prop is overriding the 'size' prop. [docs]
+     */
+    customClassName?: string | object | Array<string>;
+    /**
+     * If defined component will be rendered using 'use' tag. [docs]
+     */
+    use?: string;
+    /**
+     * Title tag content. [docs]
+     */
+    title?: string;
+}
+export declare const CIcon: React.ForwardRefExoticComponent<CIconProps & React.RefAttributes<SVGSVGElement>>;
